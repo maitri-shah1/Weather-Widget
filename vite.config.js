988d1cea8b090d02d15acf4f8dc5191ead264efd
Maitri Shah <79136731+maitri-shah1@users.noreply.github.com>
2025-02-02 14:45:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    port: process.env.PORT || 5173, // Use Render's port or fallback to 5173
+    host: "0.0.0.0", // Ensure it's accessible
+  },
   plugins: [react()],
 })
